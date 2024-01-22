35f563ab82c91d3fc19aebece53b9c46d6c067fd
nuriddinj56 <remboxussi@gmail.com>
2024-01-22 17:54:22 +0500

    add text content

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const formCreate = document.getElementById('form-create')
 const formEdit = document.getElementById('form-edit')
 const listGroupTodo = document.getElementById('list-group-todo')
-// const messageCreate = document.getElementById('message-create')
+const messageCreate = document.getElementById('message-create')
 const time = document.getElementById('time')
 const modal = document.getElementById('modal')
 const overlay = document.getElementById('overlay')
@@ -11,3 +11,23 @@ const hourEl = document.getElementById('hour')
 const minuteEl = document.getElementById('minute')
 const secondEl = document.getElementById('second')
 const closeEl = document.getElementById('close')
+
+let todos = JSON.parse(localStorage.getItem('list')) ? JSON.parse(localStorage.getItem('list')) : []
+// console.log(todos)
+
+
+// get todos
+
+formCreate.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    const todosText = formCreate['input-create'].value.trim()
+    if (todosText.length) {
+        console.log(todosText)
+    } else {
+        messageCreate.textContent = "Please,enter some text..."
+        setTimeout(() => {
+            messageCreate.textContent = ''
+        }, 2500)
+    }
+})
