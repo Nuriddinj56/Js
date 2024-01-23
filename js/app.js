050f84e5c8068818bc9e1be26579476f392dd49c
nuriddinj56 <remboxussi@gmail.com>
2024-01-23 11:43:09 +0500

    end

--- js/app.js
+++ js/app.js
@@ -22,6 +22,22 @@ function setTodos() {
     localStorage.setItem('list', JSON.stringify(todo));
 }
 
+function getTime() {
+    const now = new Date()
+    const date = now.getDate() < 10 ? now.getDate() : now.getDate()
+    const month = now.getMonth() < 10 ? '0' + (now.getMonth() + 1) : now.getMonth()
+    const year = now.getFullYear()
+    const hour = now.getHours() < 10 ? now.getHours() : now.getHours()
+    const minute = now.getMinutes() < 10 ? now.getMinutes() : now.getMinutes()
+    const second = now.getSeconds() < 10 ? now.getSeconds() : now.getSeconds()
+    fullDay.textContent = `${date}. ${month}.${year}`
+    hourEl.textContent = `${hour}`
+    minuteEl.textContent = `${minute}`
+    secondEl.textContent = `${second}`
+    return `${hour}:${minute}:${second}, ${date}.${month}.${year}`
+}
+setInterval(getTime, 1000)
+
 function showTodos() {
     const todos = JSON.parse(localStorage.getItem('list'));
     listGroupTodo.innerHTML = '';
@@ -33,7 +49,7 @@ function showTodos() {
         <div class="todo-icons">
           <span class="opacity-50 me-2">${item.time}</span>
           <img src="img/edit.svg" alt="edit icon" width="25" height="25">
-          <img src="img/delete.svg" alt="edit icon" width="25" height="25">
+          <img onclick=(deleteTodo(${i})) src="img/delete.svg" alt="edit icon" width="25" height="25">
         </div>
       </li>
     `;
@@ -54,10 +70,18 @@ formCreate.addEventListener('submit', (e) => {
     formCreate.reset();
 
     if (todoText.length) {
-        todo.push({ text: todoText, time: '18:58, 21.01.2024', completed: false });
+        todo.push({ text: todoText, time: getTime(), completed: false });
         setTodos();
         showTodos();
     } else {
         showMessage('message-create', "Please enter some text...");
     }
 });
+function deleteTodo(id) {
+    const deleteTodos = todos.filter((item, i) => {
+        return i !== id
+    })
+    todos = deleteTodos
+    setTodos()
+    showTodos()
+}
